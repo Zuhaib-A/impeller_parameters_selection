81d8fc43c819bc43be3fa35ebf9b2334da8a4f68
Zuhaib-A <83922659+Zuhaib-A@users.noreply.github.com>
2021-11-27 23:50:11 +0000

    Add files via upload

--- iterative_impeller_testing.m
+++ iterative_impeller_testing.m
@@ -4,7 +4,7 @@ clc
 format longg
 flow_rate=0.00125;
 pa_gradient=80000;
-rpm=input("Enter the RPM (2400, 2500, 2600, 2700, 2800): ");
+rpm=input("Enter the RPM (2400, 2500, 2600, 2700, 2800) ");
 r1=0.017;
 r2=0.05;
 spiral_angle=5.3;
@@ -28,8 +28,8 @@ n=input("Enter the number of blades: ");
 
 results_bf1=[];
 
-guessed_bf1=[0.01:0.01:0.99];
-guessed_bf2=[0.01:0.01:0.99];
+guessed_bf1=[0.1:0.01:0.95];
+guessed_bf2=[0.1:0.01:0.95];
 
 index=0;
 for tk=inner_blade_thick
